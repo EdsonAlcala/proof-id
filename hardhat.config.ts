da19f3867cc9a9e364295e67b7d731e91b08c67d
franjoespejo <franjoespejo@gmail.com>
2022-09-18 01:07:40 +0200

    deployed

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import type { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox"
+import "dotenv/config"
 
+
+const { API_URL, PRIVATE } = process.env;
+console.log(PRIVATE)
 const config: HardhatUserConfig = {
-  solidity: "0.8.17"
+  solidity: "0.8.17",
+   networks: {
+      goerli: {
+         url: API_URL,
+         accounts: [`${PRIVATE}`]
+      }
+   }
 };
 
 export default config;
